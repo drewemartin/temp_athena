3219ed5f73e9e4c4f2beaa38d3ec6b096b062d17
Elbert Saddler <esaddler@agora.org>
2013-09-24 16:37:19 -0400

    changes overall mode to withdrawn when withdrawn.  Changes overall mode to synchronous when reinstated

--- system/tables/STUDENT.rb
+++ system/tables/STUDENT.rb
@@ -223,6 +223,14 @@ end
             record.fields["withdrawreason"     ].value = nil        
             record.save
             
+            sid = record.fields["student_id"].value
+            
+            attendance_mode_record = $tables.attach("student_attendance_mode").by_studentid(sid)
+            if attendance_mode_record
+                attendance_mode_record.fields["attendance_mode"].value = "Synchronous"
+                attendance_mode_record.save
+            end
+            
         } if pids
         
     end
