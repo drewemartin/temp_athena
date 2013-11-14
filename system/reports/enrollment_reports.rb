a63ce0a3107923372f4e280e758bf797621792e1
Elbert Saddler <esaddler@agora.org>
2013-11-14 15:15:40 -0500

    if the document type wasnt a table, it would never have a nicename.  Was a problem for the duplicated students report

--- system/reports/enrollment_reports.rb
+++ system/reports/enrollment_reports.rb
@@ -38,7 +38,7 @@ class Enrollment_Reports
         rows = $db.get_data(sql_string)
         
         if rows
-            file_path = $reports.save_document({:csv_rows=>rows.insert(0,headers), :category_name=>"Enrollment", :type_name=>"Duplicated Students Report"})
+            file_path = $reports.save_document({:csv_rows=>rows.insert(0,headers), :category_name=>"Enrollment", :type_name=>"duplicated_students_report"})
             $reports.move_to_athena_reports_from_docs(file_path, location, filename, false)
             return file_path
         else
