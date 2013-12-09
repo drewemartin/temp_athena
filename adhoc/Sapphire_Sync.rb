7756acd8fc1e32cc85b7129838124c1a59ab5cfb
Jenifer Halverson <jhalverson@agora.org>
2013-12-09 13:14:21 -0500

    UPDATE - accounts for ethnicity reporting differences.

--- adhoc/Sapphire_Sync.rb
+++ adhoc/Sapphire_Sync.rb
@@ -9,16 +9,18 @@ class Sapphire_Sync < Base
         
         mapped_fields = {
           
-          "studentfirstname"	=>        "first_name"      ,
-          "mailingstate"	=>        "address_state"   ,
-          "studentlastname"	=>        "last_name"       ,
-          "preferred_name"	=>        "other_name"      ,
-          "mailingaddress1"	=>        "address_1"       ,
-          "mailingaddress2"	=>        "address_2"       ,
-          "mailingcity"	        =>        "address_city"    ,
+          #"mailingstate"	=>        "address_state"   ,
+          #"mailingaddress1"	=>        "address_1"       ,
+          #"mailingaddress2"	=>        "address_2"       ,
+          #"mailingcity"        =>        "address_city"    ,
+          #"mailingzip"	        =>        "address_zip"     ,
+          
           "studentgender"	=>        "gender"          ,
           "studenthomephone"	=>        "phone_no"        ,
-          "mailingzip"	        =>        "address_zip"
+          "preferred_name"	=>        "other_name"      ,
+          "studentfirstname"	=>        "first_name"      ,
+          "studentlastname"	=>        "last_name"       ,
+          "ethnicity"           =>        "ethnicity"  
            
         }
         
@@ -35,12 +37,24 @@ class Sapphire_Sync < Base
                     s_field = $tables.attach("SAPPHIRE_STUDENT_DEMOGRAPHICS").field_value(mapped_fields[field_name],  "WHERE student_id = '#{sid}'")
                     a_field = $tables.attach("STUDENT"                      ).field_value(field_name,                 "WHERE student_id = '#{sid}'")
                     
+                    fields_match            = s_field == a_field
+                    parsed_phone_matches    = (!s_field.nil? && !a_field.nil?) && s_field.gsub('-','').gsub('(','').gsub(')','').gsub(' ','') == a_field.gsub('-','').gsub('(','').gsub(')','').gsub(' ','')
+                    male_match              = s_field == "M" && a_field == "Male"
+                    female_match            = s_field == "F" && a_field == "Female"
+                    ethnicity_match         = (
+                        s_field == "1"  && a_field == "American Indian"                                                                 ||
+                        s_field == "9"  && a_field == "Asian"                                                                           ||
+                        s_field == "3"  && a_field == "African-American"                                                                ||
+                        s_field == "10" && (a_field == "Native Hawaiian or other Pacific Islander" || a_field == "Pacific Islander")    ||
+                        s_field == "4"  && a_field == "Hispanic"                                                                        ||
+                        s_field == "6"  && a_field == "Multi-racial"                                                                    ||
+                        s_field == "7"  && (a_field == "Declined to State" || a_field == "Undefined")                                   ||
+                        s_field == "5"  && a_field == "White"                                                                           
+                    )
+                    
                     if !(
                      
-                        s_field                                                         == a_field                                                      ||
-                        (!s_field.nil? && !a_field.nil?) && s_field.gsub('-','').gsub('(','').gsub(')','').gsub(' ','') == a_field.gsub('-','').gsub('(','').gsub(')','').gsub(' ','')  ||
-                        s_field                                                         == "M" && a_field == "Male"                                     ||
-                        s_field                                                         == "F" && a_field == "Female"
+                        fields_match || parsed_phone_matches || male_match || female_match || ethnicity_match
                         
                     )
                       
