1c5a888dee4bb40c91173108162457c4815f993d
Jenifer Halverson <jhalverson@agora.org>
2013-12-23 14:27:14 -0500

    UPDATE - Added student_leap table to support the upcoming "LEAP" project in Athena.

---
+++ system/tables/STUDENT_LEAP.rb
@@ -0,0 +1,62 @@
+#!/usr/local/bin/ruby
+require "#{$paths.base_path}athena_table"
+
+class STUDENT_LEAP < Athena_Table
+    
+    #---------------------------------------------------------------------------
+    def initialize()
+        super()
+        @table_structure = nil
+    end
+    #---------------------------------------------------------------------------
+   
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+public
+def xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxPUBLIC_METHODS
+end
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+def x______________TRIGGER_EVENTS
+end
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+def x______________VALIDATION
+end
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+private
+def xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxPRIVATE_METHODS
+end
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    
+    def table
+        if !@table_structure
+            structure_hash = {
+                "name"              => "student_leap",
+                "file_name"         => "student_leap.csv",
+                "file_location"     => "student_leap",
+                "source_address"    => nil,
+                "source_type"       => nil,
+                "audit"             => true,
+                :relationship       => :one_to_one
+            }
+            @table_structure = set_fields(structure_hash)
+        end
+        return @table_structure
+    end
+    
+    def set_fields(structure_hash)
+        field_order = Array.new
+        structure_hash["fields"] = Hash.new
+            
+            structure_hash["fields"]["student_id"] = {"data_type"=>"int", "file_field"=>"student_id"} if field_order.push("student_id")
+            structure_hash["fields"]["leap_level"] = {"data_type"=>"int", "file_field"=>"leap_level"} if field_order.push("leap_level")
+            
+        structure_hash["field_order"] = field_order
+        return structure_hash
+    end
+
+end
