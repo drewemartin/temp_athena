b9f7fc15609703009ab9c72e9031cef0209b3254
Jenifer Halverson <jhalverson@agora.org>
2014-02-26 16:01:47 -0500

    TEST EVENTS IN PROGRESS

--- system/web_interfaces/Test_Event_Site_Web.rb
+++ system/web_interfaces/Test_Event_Site_Web.rb
@@ -148,7 +148,7 @@ end
     def response
         
         if field = $kit.rows.first[1].fields["serial_number"]
-            if !field.updated
+            if field.updated == false
                 student_id = $tables.attach("TEST_PACKETS").field_value("student_id", "WHERE serial_number = '#{field.value}'")
                 $kit.web_error.duplicate_assignment_error(
                     additional_message="This test packet is already assigned to the student with ID# #{student_id}. You must unassign this packet first.<BR>"
