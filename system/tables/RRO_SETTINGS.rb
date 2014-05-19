4e9546af50922a749b0336c10f1087564a912a6a
Jenifer Halverson <jhalverson@agora.org>
2014-05-19 13:34:01 -0400

    INITIAL ADD - RRO SETTINGS

---
+++ system/tables/RRO_SETTINGS.rb
@@ -0,0 +1,62 @@
+#!/usr/local/bin/ruby
+require "#{$paths.base_path}athena_table"
+
+class RRO_SETTINGS < Athena_Table
+    
+    #---------------------------------------------------------------------------
+    def initialize()
+        super()
+        @table_structure = nil
+    end
+    #---------------------------------------------------------------------------
+   
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+public
+def xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxPUBLIC_METHODS
+end
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+def x______________TRIGGER_EVENTS
+end
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+def x______________VALIDATION
+end
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+private
+def xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxPRIVATE_METHODS
+end
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    
+    def table
+        if !@table_structure
+            structure_hash = {
+                :data_base          => "#{$config.school_name}_master",
+                "name"              => "rro_settings",
+                "file_name"         => "rro_settings.csv",
+                "file_location"     => "rro_settings",
+                "source_address"    => nil,
+                "source_type"       => nil,
+                "audit"             => true
+            }
+            @table_structure = set_fields(structure_hash)
+        end
+        return @table_structure
+    end
+    
+    def set_fields(structure_hash)
+        field_order = Array.new
+        structure_hash["fields"] = Hash.new
+            
+            structure_hash["fields"]["expiration_interval_unit"     ] = {"data_type"=>"text",   "file_field"=>"expiration_interval_unit"    } if field_order.push("expiration_interval_unit"    )
+            structure_hash["fields"]["expiration_interval_number"   ] = {"data_type"=>"int",    "file_field"=>"expiration_interval_number"  } if field_order.push("expiration_interval_number"  )
+            
+        structure_hash["field_order"] = field_order
+        return structure_hash
+    end
+
+end
