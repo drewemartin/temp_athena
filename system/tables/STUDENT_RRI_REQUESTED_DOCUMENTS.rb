8744375cdac97c76b0492f8fed0fc47de6edc806
Jenifer Halverson <jhalverson@agora.org>
2014-06-13 15:20:41 -0400

    UPDATE - notes field added

--- system/tables/STUDENT_RRI_REQUESTED_DOCUMENTS.rb
+++ system/tables/STUDENT_RRI_REQUESTED_DOCUMENTS.rb
@@ -139,6 +139,7 @@ end
             structure_hash["fields"]["record_type_id"   ] = {"data_type"=>"int",  "file_field"=>"record_type_id"    } if field_order.push("record_type_id"  )
             structure_hash["fields"]["status"           ] = {"data_type"=>"text", "file_field"=>"status"            } if field_order.push("status"          )
             structure_hash["fields"]["date_completed"   ] = {"data_type"=>"date", "file_field"=>"date_completed"    } if field_order.push("date_completed"  )
+            structure_hash["fields"]["notes"            ] = {"data_type"=>"text", "file_field"=>"notes"             } if field_order.push("notes"           )
             
         structure_hash["field_order"] = field_order
         return structure_hash
