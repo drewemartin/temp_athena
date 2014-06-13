ac8aabcb4ff3d3b4f7593c03a2400b083dcca368
Jenifer Halverson <jhalverson@agora.org>
2014-06-13 13:40:32 -0400

    UPDATE - overall status and completed date will not be needed

--- system/tables/STUDENT_RRI_REQUESTS.rb
+++ system/tables/STUDENT_RRI_REQUESTS.rb
@@ -63,9 +63,7 @@ end
             structure_hash["fields"]["request_method"   ] = {"data_type"=>"text", "file_field"=>"request_method"    } if field_order.push("request_method"  )
             structure_hash["fields"]["notes"            ] = {"data_type"=>"text", "file_field"=>"notes"             } if field_order.push("notes"           )
             structure_hash["fields"]["priority_level"   ] = {"data_type"=>"bool", "file_field"=>"priority_level"    } if field_order.push("priority_level"  )
-            structure_hash["fields"]["status"           ] = {"data_type"=>"text", "file_field"=>"status"            } if field_order.push("status"          )
-            structure_hash["fields"]["date_completed"   ] = {"data_type"=>"date", "file_field"=>"date_completed"    } if field_order.push("date_completed"  )
-        
+            
         structure_hash["field_order"] = field_order
         return structure_hash
     end
